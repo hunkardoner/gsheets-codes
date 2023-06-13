a9ae108ed4e083c1832e5651b5a1366130075d7b
Hünkar <45366700+hunkardoner@users.noreply.github.com>
2023-06-13 21:24:11 +0300

    Update by-word-count.js

--- by-word-count.js
+++ by-word-count.js
@@ -23,24 +23,24 @@ function colorRows() {
 // If you want it to include a certain long tail range and a word you specify (like example), you can use it.
   
 function colorRows() {
-    var sheet = SpreadsheetApp.getActiveSheet();
-    var range = sheet.getDataRange();
-  //   var range = sheet.getRange("A1:A500");
-    var values = range.getValues();
-    var color;
-    
-    for (var i=1; i<values.length; i++) {
-      if (values[i][0].toLowerCase().includes("example")) {
-        color = "#ff0000"; // kırmızı
-      } else if (values[i][0].split(" ").length >= 5) {
-        color = "#b7e1cd"; // yeşil
-      } else if (values[i][0].split(" ").length == 4) {
-        color = "#fcd5b4"; // turuncu
-      } else if (values[i][0].split(" ").length == 3) {
-        color = "#fff2cc"; // sarı
-      } else {
-        color = "#ffffff"; // beyaz
-      }
-      sheet.getRange(i+1,1).setBackground(color);
+  var sheet = SpreadsheetApp.getActiveSheet();
+  var range = sheet.getDataRange();
+//   var range = sheet.getRange("A1:A500");
+  var values = range.getValues();
+  var color;
+  
+  for (var i=1; i<values.length; i++) {
+    if (values[i][0].toLowerCase().includes("example") && values[i][0].split(" ").length >= 6) {
+      color = "#ff0000"; // kırmızı
+    } else if (values[i][0].toLowerCase().includes("example") && values[i][0].split(" ").length == 5) {
+      color = "#b7e1cd"; // yeşil
+    } else if (values[i][0].toLowerCase().includes("example") && values[i][0].split(" ").length == 4) {
+      color = "#fcd5b4"; // turuncu
+    } else if (values[i][0].toLowerCase().includes("example") && values[i][0].split(" ").length == 3) {
+      color = "#fff2cc"; // sarı
+    } else {
+      color = "#ffffff"; // beyaz
     }
+    sheet.getRange(i+1,1).setBackground(color);
   }
+}
